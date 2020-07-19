d425cddf0b7684aa7c2e628d35fbfc827c76e351
Ezequiel Bertran <enbertran@gmail.com>
2020-07-19 17:17:58 -0300

    local db access

--- dbutils.php
+++ dbutils.php
@@ -3,13 +3,13 @@
 function db_connect()
 {
  //$db = mysql_pconnect("localhost", "root", "mynorthstock");
- $db = mysql_pconnect("localhost", "root", "");
+ $db = mysql_pconnect("localhost", "suda", "suda");
  if (!$db)
  {
   echo "Error: No es posible conectar al motor de base de datos." . mysql_error();
   exit;
  }
- if ( !mysql_select_db("north") )
+ if ( !mysql_select_db("north_asus") )
  {
   echo "Error: No es posible seleccionar la base de datos." . mysql_error();
   exit;
